95b89f8271fd96e1b58965aba8737ddd6b6a2f47
Jesse Stuart <jdstuart@icloud.com>
2017-12-29 15:29:48 -0500

    [commit message skipped]

--- .circleci/provision.sh
+++ .circleci/provision.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -ex
+
 EXTPACK_NAME="Oracle_VM_VirtualBox_Extension_Pack-4.3.36.vbox-extpack"
 
 # Add apt-key sources for virtualbox.
@@ -13,6 +15,6 @@ apt-get update -y
 apt-get install -y linux-headers-$(uname -r) build-essential virtualbox dkms python ansible
 
 wget http://download.virtualbox.org/virtualbox/4.3.36/$EXTPACK_NAME -O /tmp/$EXTPACK_NAME
-VBoxManage extpack install "$EXTPACK_NAME"
+VBoxManage extpack install "/tmp/$EXTPACK_NAME"
 wget https://releases.hashicorp.com/vagrant/2.0.1/vagrant_2.0.1_x86_64.deb -O /tmp/vagrant_2.0.1_x86_64.deb
-dpkg -i vagrant_2.0.1_x86_64.deb
+dpkg -i /tmp/vagrant_2.0.1_x86_64.deb
