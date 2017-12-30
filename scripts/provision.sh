ca4e374b4ae1c87937be7249985cf1155fbbb77b
Jesse Stuart <jdstuart@icloud.com>
2017-12-30 17:48:46 -0500

    [commit message skipped]

---
+++ scripts/provision.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -ex
+
+EXTPACK_NAME="Oracle_VM_VirtualBox_Extension_Pack-4.3.36.vbox-extpack"
+
+# Add apt-key sources for virtualbox.
+apt-get -y update
+apt-get -y install sudo wget curl
+wget -q -O - http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc | apt-key add -
+echo "deb http://download.virtualbox.org/virtualbox/debian precise non-free contrib" >>/etc/apt/sources.list.d/virtualbox.org.lists
+# Install the bare-minimum dependencies to allow us to connect
+# via Ansible, even in CI.
+apt-get update -y
+apt-get install -y linux-headers-$(uname -r) build-essential virtualbox dkms python ansible
+
+wget http://download.virtualbox.org/virtualbox/4.3.36/$EXTPACK_NAME -O /tmp/$EXTPACK_NAME
+VBoxManage extpack install "/tmp/$EXTPACK_NAME"
+wget https://releases.hashicorp.com/vagrant/2.0.1/vagrant_2.0.1_x86_64.deb -O /tmp/vagrant_2.0.1_x86_64.deb
+dpkg -i /tmp/vagrant_2.0.1_x86_64.deb
